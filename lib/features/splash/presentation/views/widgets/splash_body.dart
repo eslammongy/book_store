2f685d7aec5237e997ec27c44860b280cd74d352
eslam <eslammongy995@gmail.com>
2023-02-02 20:49:51 +0200

    improve splash screen ui

--- lib/features/splash/presentation/views/widgets/splash_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_body.dart
@@ -1,5 +1,5 @@
 import 'package:book_store/core/utils/assets_manager.dart';
-import 'package:book_store/core/utils/constants/app_colors.dart';
+import 'package:book_store/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -7,25 +7,36 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Image.asset(
-            AppAssets.logoImage,
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [blackColor, greyColor],
+        begin: Alignment.bottomLeft,
+        end: Alignment.topRight,
+      )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              AppAssets.logoImage,
+              width: 200,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-        Center(
-          child: Text(
-            "Bookly",
-            style: TextStyle(
-                color: greenColor, fontWeight: FontWeight.w700, fontSize: 35),
+          const SizedBox(
+            height: 15,
           ),
-        )
-      ],
+          Center(
+            child: Text(
+              "E-Booky",
+              style: TextStyle(
+                  color: darkOrangeColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 45),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:book_store/core/utils/constants/app_colors.dart';
+import 'package:book_store/core/constants/app_colors.dart';
 import 'package:book_store/features/splash/presentation/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
