e8d6af105f33efab2daaae82e316d3f34da9b022
eslam <eslammongy995@gmail.com>
2023-02-06 11:06:58 +0200

    add title & subtitle to book details

--- lib/features/home/presentation/views/widgets/book_details_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_body.dart
@@ -1,3 +1,7 @@
+import 'package:book_store/core/constants/app_colors.dart';
+import 'package:book_store/core/constants/text_style.dart';
+import 'package:book_store/features/home/presentation/views/book_details.dart';
+import 'package:book_store/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:book_store/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:book_store/features/home/presentation/views/widgets/featured_books_listview.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +19,33 @@ class BooKDetailsBody extends StatelessWidget {
         child: Column(
           children: [
             const CustomDetailsAppBar(),
+            const SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.18),
               child: const CustomBookImage(),
-            )
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text("System Design Interview",
+                style: AppTextStyle.textStyle30.copyWith(
+                  color: greenColor,
+                  fontWeight: FontWeight.w700,
+                )),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: 0.7,
+              child: Text("Alex xue & Sahn lam",
+                  style: AppTextStyle.textStyle18.copyWith(
+                      color: greenColor,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic)),
+            ),
+            BookRatingRow()
           ],
         ),
       ),
