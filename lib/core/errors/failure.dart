5350da25c6cadf2789949d4a503c48ccd2c33626
eslam <eslammongy995@gmail.com>
2023-02-08 18:08:22 +0200

    build home repo methods and failure file

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+abstract class Failure {}
+
+class ServerError extends Failure {}
+/* class ServerError extends Failure{}
+class ServerError extends Failure{}
+class ServerError extends Failure{} */
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:book_store/core/errors/failure.dart';
+import 'package:book_store/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
