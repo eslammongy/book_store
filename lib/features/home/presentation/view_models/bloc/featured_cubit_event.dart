501cf4f7d504ce0ef7593b74cd53c6c6419f91fa
eslam <eslammongy995@gmail.com>
2023-02-09 13:53:23 +0200

    create newest books cubit

--- lib/features/home/presentation/view_models/bloc/featured_cubit_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'featured_cubit_bloc.dart';
-
-abstract class FeaturedCubitEvent extends Equatable {
-  const FeaturedCubitEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- lib/features/home/presentation/view_models/featured_cubit/featured_cubit_bloc.dart
+++ lib/features/home/presentation/view_models/featured_cubit/featured_cubit_bloc.dart
@@ -1,12 +1,7 @@
-import 'dart:ffi';
-
-import 'package:bloc/bloc.dart';
 import 'package:book_store/features/home/data/models/book_model/book_model.dart';
 import 'package:book_store/features/home/data/repos/home_repo.dart';
+import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/animation.dart';
-
-part 'featured_cubit_event.dart';
 part 'featured_cubit_state.dart';
 
 class FeaturedCubitBloc extends Cubit<FeaturedCubitState> {
