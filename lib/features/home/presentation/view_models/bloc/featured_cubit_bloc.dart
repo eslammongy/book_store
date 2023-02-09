61c74c99d1d6609f6df72c29329cbfcf9b227bf1
eslam <eslammongy995@gmail.com>
2023-02-09 13:32:57 +0200

    create featured books cubit

--- lib/features/home/presentation/view_models/bloc/featured_cubit_bloc.dart
+++ lib/features/home/presentation/view_models/bloc/featured_cubit_bloc.dart
@@ -1,14 +1,27 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:book_store/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/animation.dart';
 
 part 'featured_cubit_event.dart';
 part 'featured_cubit_state.dart';
 
-class FeaturedCubitBloc extends Bloc<FeaturedCubitEvent, FeaturedCubitState> {
-  FeaturedCubitBloc() : super(FeaturedCubitInitial()) {
-    on<FeaturedCubitEvent>((event, emit) {
-      // TODO: implement event handler
+class FeaturedCubitBloc extends Cubit<FeaturedCubitState> {
+  FeaturedCubitBloc(this.homeRepo) : super(FeaturedCubitInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedCubitLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold((failure) {
+      emit(FeaturedCubitFailure(failure.errorMsg));
+    }, (books) {
+      emit(FeaturedCubitSuccess(books));
     });
   }
 }
